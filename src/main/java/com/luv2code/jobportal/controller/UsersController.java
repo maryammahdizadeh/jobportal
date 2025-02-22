fcad0e36c69cbddff9ae1ef0d324e26505538775
maryam <maryam19.mahdizadeh@gmail.com>
2025-02-22 19:27:20 +0330

    Add request mapping for register new user

--- src/main/java/com/luv2code/jobportal/controller/UsersController.java
+++ src/main/java/com/luv2code/jobportal/controller/UsersController.java
@@ -3,10 +3,12 @@ package com.luv2code.jobportal.controller;
 import com.luv2code.jobportal.entity.Users;
 import com.luv2code.jobportal.entity.UsersType;
 import com.luv2code.jobportal.service.UsersTypeSevice;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -26,5 +28,11 @@ public class UsersController {
         model.addAttribute("getAllTypes", usersTypes);
         model.addAttribute("user", new Users());
         return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users) {
+        System.out.println("User:: "+users);
+        return "dashboard";
     }
 }
