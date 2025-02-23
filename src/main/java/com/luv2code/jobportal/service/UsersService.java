4f6514b8c6050b7e84fe25c71fb14eea95593075
maryam <maryam19.mahdizadeh@gmail.com>
2025-02-23 09:30:53 +0330

    Add UsersService and create method for add new user

---
+++ src/main/java/com/luv2code/jobportal/service/UsersService.java
@@ -0,0 +1,25 @@
+package com.luv2code.jobportal.service;
+
+import com.luv2code.jobportal.entity.Users;
+import com.luv2code.jobportal.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public Users addNew(Users user) {
+        user.setActive(true);
+        user.setRegistrationDate(new Date(System.currentTimeMillis()));
+        return usersRepository.save(user);
+    }
+}
