79c895d652947c3d33bac66a98ebf0ce573dfb94
maryam <maryam19.mahdizadeh@gmail.com>
2025-02-23 12:53:10 +0330

    Create JPA Entity: JobSeekerProfile

---
+++ src/main/java/com/luv2code/jobportal/entity/Skills.java
@@ -0,0 +1,11 @@
+package com.luv2code.jobportal.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jdk.jfr.Enabled;
+
+@Entity
+@Table(name = "skills")
+public class Skills {
+}
