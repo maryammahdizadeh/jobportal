99041ac941db783e3f269c8168726453dead49da
maryam <maryam19.mahdizadeh@gmail.com>
2025-04-20 00:07:27 +0330

    Create addNew method for post new job

--- src/main/java/com/luv2code/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/luv2code/jobportal/controller/JobPostActivityController.java
@@ -1,6 +1,8 @@
 package com.luv2code.jobportal.controller;
 
 import com.luv2code.jobportal.entity.JobPostActivity;
+import com.luv2code.jobportal.entity.Users;
+import com.luv2code.jobportal.service.JobPostActivityService;
 import com.luv2code.jobportal.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -9,15 +11,20 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
 
 @Controller
 public class JobPostActivityController {
 
     private final UsersService usersService;
+    private final JobPostActivityService jobPostActivityService;
 
     @Autowired
-    public JobPostActivityController(UsersService usersService) {
+    public JobPostActivityController(UsersService usersService, JobPostActivityService jobPostActivityService) {
         this.usersService = usersService;
+        this.jobPostActivityService = jobPostActivityService;
     }
 
     @GetMapping("/dashboard/")
@@ -41,5 +48,18 @@ public class JobPostActivityController {
         model.addAttribute("user", usersService.getCurrentUserProfile());
 
         return "add-jobs";
+    }
+
+    @PostMapping("/dashboard/addNew")
+    public String addNew(JobPostActivity jobPostActivity, Model model) {
+
+        Users user = usersService.getCurrentUser();
+        if (user != null) {
+            jobPostActivity.setPostedById(user);
+        }
+        jobPostActivity.setPostedDate(new Date());
+        model.addAttribute("jobPostActivity", jobPostActivity);
+        JobPostActivity saved = jobPostActivityService.addNew(jobPostActivity);
+        return "redirect:/dashboard/";
     }
 }
