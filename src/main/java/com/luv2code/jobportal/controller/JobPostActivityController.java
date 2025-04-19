71777f870864f1d652d7ca8bef7b0cc6363d04fb
maryam <maryam19.mahdizadeh@gmail.com>
2025-04-19 20:37:21 +0330

    Enhance JobPostActivityController for posting new job

--- src/main/java/com/luv2code/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/luv2code/jobportal/controller/JobPostActivityController.java
@@ -1,5 +1,6 @@
 package com.luv2code.jobportal.controller;
 
+import com.luv2code.jobportal.entity.JobPostActivity;
 import com.luv2code.jobportal.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -32,5 +33,13 @@ public class JobPostActivityController {
         model.addAttribute("user", currentUserProfile);
 
         return "dashboard";
+    }
+
+    @GetMapping("/dashboard/add")
+    public String addJobs(Model model) {
+        model.addAttribute("jobPostActivity", new JobPostActivity());
+        model.addAttribute("user", usersService.getCurrentUserProfile());
+
+        return "add-jobs";
     }
 }
