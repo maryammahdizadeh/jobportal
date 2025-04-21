36c9e05b9c72178671b165645a0d8b06be308d20
maryam <maryam19.mahdizadeh@gmail.com>
2025-04-21 19:26:55 +0330

    Create JobSeekerProfileService and add getOne method to find jobSeekers by their id from database

---
+++ src/main/java/com/luv2code/jobportal/service/JobSeekerProfileService.java
@@ -0,0 +1,23 @@
+package com.luv2code.jobportal.service;
+
+import com.luv2code.jobportal.entity.JobSeekerProfile;
+import com.luv2code.jobportal.repository.JobSeekerProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class JobSeekerProfileService {
+
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+
+    @Autowired
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+    }
+
+    public Optional<JobSeekerProfile> getOne(Integer id) {
+        return jobSeekerProfileRepository.findById(id);
+    }
+}
