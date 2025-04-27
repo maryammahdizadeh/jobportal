43f612aee89c34245124ccd3611479610b721d23
maryam <maryam19.mahdizadeh@gmail.com>
2025-04-27 20:53:15 +0330

    Update JobPostActivityController for saving a job

--- src/main/java/com/luv2code/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/luv2code/jobportal/controller/JobPostActivityController.java
@@ -3,6 +3,7 @@ package com.luv2code.jobportal.controller;
 import com.luv2code.jobportal.entity.*;
 import com.luv2code.jobportal.service.JobPostActivityService;
 import com.luv2code.jobportal.service.JobSeekerApplyService;
+import com.luv2code.jobportal.service.JobSeekerSaveService;
 import com.luv2code.jobportal.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -29,12 +30,14 @@ public class JobPostActivityController {
     private final UsersService usersService;
     private final JobPostActivityService jobPostActivityService;
     private final JobSeekerApplyService jobSeekerApplyService;
+    private final JobSeekerSaveService jobSeekerSaveService;
 
     @Autowired
-    public JobPostActivityController(UsersService usersService, JobPostActivityService jobPostActivityService, JobSeekerApplyService jobSeekerApplyService) {
+    public JobPostActivityController(UsersService usersService, JobPostActivityService jobPostActivityService, JobSeekerApplyService jobSeekerApplyService, JobSeekerSaveService jobSeekerSaveService) {
         this.usersService = usersService;
         this.jobPostActivityService = jobPostActivityService;
         this.jobSeekerApplyService = jobSeekerApplyService;
+        this.jobSeekerSaveService = jobSeekerSaveService;
     }
 
     @GetMapping("/dashboard/ ")
@@ -117,7 +120,42 @@ public class JobPostActivityController {
                         currentUserProfile).getUserAccountId());
                 model.addAttribute("jobPost", recruiterJobs);
             } else {
-                List<JobSeekerApply> jobSeekerApplyList = jobSeekerApplyService.getCandidatesJobs((JobSeekerProfile) currentUserProfile);
+                List<JobSeekerApply> jobSeekerApplyList = jobSeekerApplyService
+                        .getCandidatesJobs((JobSeekerProfile) currentUserProfile);
+                List<JobSeekerSave> jobSeekerSaveList = jobSeekerSaveService
+                        .getCandidatesJob((JobSeekerProfile) currentUserProfile);
+
+                boolean exist;
+                boolean saved;
+
+                for (JobPostActivity jobActivity : jobPost) {
+                    exist = false;
+                    saved = false;
+                    for (JobSeekerApply jobSeekerApply : jobSeekerApplyList) {
+                        if (Objects.equals(jobActivity.getJobPostId(), jobSeekerApply.getJob().getJobPostId())) {
+                            jobActivity.setActive(true);
+                            exist = true;
+                            break;
+                        }
+                    }
+
+                    for (JobSeekerSave jobSeekerSave : jobSeekerSaveList) {
+                        if (Objects.equals(jobActivity.getJobPostId(), jobSeekerSave.getJob().getJobPostId())) {
+                            jobActivity.setSaved(true);
+                            saved = true;
+                            break;
+
+                        }
+                    }
+                    if (!exist) {
+                        jobActivity.setActive(false);
+                    }
+                    if (!saved) {
+                        jobActivity.setSaved(false);
+                    }
+
+                    model.addAttribute("jobPost", jobPost);
+                }
             }
         }
         model.addAttribute("user", currentUserProfile);
