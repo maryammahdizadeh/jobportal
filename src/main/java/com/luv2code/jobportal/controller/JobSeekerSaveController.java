f3dbd2e8019f26f7901d1971d66aea837656c0a0
maryam <maryam19.mahdizadeh@gmail.com>
2025-04-29 19:34:35 +0330

    Add request mapping method for show list of saved jobs to JobSeekerSaveController

--- src/main/java/com/luv2code/jobportal/controller/JobSeekerSaveController.java
+++ src/main/java/com/luv2code/jobportal/controller/JobSeekerSaveController.java
@@ -13,6 +13,8 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -49,7 +51,12 @@ public class JobSeekerSaveController {
             }
             jobSeekerSaveService.addNew(jobSeekerSave);
         }
-
         return "redirect:/dashboard/";
+    }
+
+    @GetMapping("saved-jobs/")
+    public String savedJobs(Model model) {
+
+        return "saved-jobs";
     }
 }
