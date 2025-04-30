b9b0af5d825ba35b2c55fa5152e1553308293d3c
maryam <maryam19.mahdizadeh@gmail.com>
2025-04-30 19:18:26 +0330

    Add request mapping method to show the profile for given candidate id

--- src/main/java/com/luv2code/jobportal/controller/JobSeekerProfileController.java
+++ src/main/java/com/luv2code/jobportal/controller/JobSeekerProfileController.java
@@ -16,10 +16,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -116,5 +113,11 @@ public class JobSeekerProfileController  {
 
          return "redirect:/dashboard/";
 
+    }
+
+    @GetMapping("/{id}")
+    public String candidateProfile(@PathVariable("id") int id, Model model) {
+
+        return "job-seeker-profile";
     }
 }
